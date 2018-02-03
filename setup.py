b2f6ab14b27ca5bc365e1090393bd641cef7c7e1
rbn920 <robertb.nelson@gmail.com>
2018-02-03 14:36:19 -0800

    Change setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
 setup(name='bh100',
-      version='0.2',
+      version='0.1',
+      py_modules='bh100'
       description='Generates a JSON file of the top Crypto in the market and where you can buy them.',
-      scripts='bh100',
       url='https://github.com/rbn920/bh100',
       author='Robert Nelson',
       author_email='robertb.nelson@gmail.com',
+      install_requires=['click',
+                        'coinmarketcap',
+                        'requests',
+                        'beautifulsoup4']
+      python_requires='>=3'
+      entry_points='''[console_scripts]bh100=bh100:main''''
       license='MIT')
