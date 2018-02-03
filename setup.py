5543b55ef1231a351835b44a8c42575f7d06d185
rbn920 <robertb.nelson@gmail.com>
2018-02-03 14:05:57 -0800

    Add script setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='bh100',
       version='0.1',
       description='Generates a JSON file of the top Crypto in the market and where you can buy them.',
+      scripts='bh100',
       url='https://github.com/rbn920/bh100',
       author='Robert Nelson',
       author_email='robertb.nelson@gmail.com',
