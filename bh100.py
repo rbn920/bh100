adf8236a924d9e2157773a2c1d6ca91d32ce7d92
rbn920 <robertb.nelson@gmail.com>
2018-02-04 14:20:32 -0800

    Refactor and add 'prefered exchange' fuctionality

--- bh100.py
+++ bh100.py
@@ -7,18 +7,18 @@ import json
 from collections import OrderedDict
 
 
-def get_coins(quantity):
-    market = coinmarketcap.Market()
-    coins = market.ticker(start=0, limit=quantity)
+class Coin:
+    def __init__(self, cmc_id, name, rank, symbol):
+        self.id = cmc_id
+        self.name = name
+        self.rank = rank
+        self.symbol = symbol
+        self.url = 'https://coinmarketcap.com/currencies/{}/'.format(cmc_id)
+        self.get_exchanges()
 
-    return [{k: coin[k] for k in ('id', 'name', 'rank', 'symbol')} for coin in coins]
-
-
-def get_markets(coin, quantity):
-    url = 'https://coinmarketcap.com/currencies/{}/'.format(coin)
-    page = requests.get(url)
-    if page.status_code == 200:
-        c = page.content
+    def get_exchanges(self):
+        cmc_page = requests.get(self.url)
+        c = cmc_page.content
         soup = BeautifulSoup(c, 'html.parser')
         table = soup.find('table', id='markets-table')
         table_body = table.find('tbody')
@@ -26,34 +26,76 @@ def get_markets(coin, quantity):
         exchanges = []
         for row in rows:
             exchange = row.find_all('a', {'href': re.compile(r'/exchanges/*')})
-            exchanges.append(exchange[0].text)
+            exchanges.append(exchange[0].text.lower())
 
-#        for row in rows[:quantity]:
-#             exchange = row.find_all('a', {'href': re.compile(r'/exchanges/*')})
-#             exchanges.append(exchange[0].text)
+        dedup = list(OrderedDict.fromkeys(exchanges))
+        self.exchanges = dedup
 
-    else:
-        print('Error reaching page')
-        exchanges = ['Error']
 
-    return list(OrderedDict.fromkeys(exchanges))[:quantity]
+class Exchange:
+    def __init__(self, name, preference_rank):
+        self.name = name
+        self.preference_rank = preference_rank
+        self.coins = []
+
+    def in_exchange(self, coin):
+        if self.name in coin.exchanges:
+            self.coins.append(coin.name)
+
+
+def get_coins(quantity):
+    market = coinmarketcap.Market()
+    coins = market.ticker(start=0, limit=quantity)
+
+    return [Coin(coin['id'],
+                 coin['name'],
+                 coin['rank'],
+                 coin['symbol']) for coin in coins]
+
+
+def to_json(items, file_name, coins=True):
+    as_dicts =[]
+    for item in items:
+        as_dict = item.__dict__
+        if coins:
+            as_dict.pop('id')
+
+        as_dicts.append(as_dict)
+
+    with open(file_name, 'w') as fp:
+        json.dump(as_dicts, fp)
+
+    return
+
+
+def get_prefered_exchanges(prefered):
+    exchanges = []
+    for rank, exchange in enumerate(prefered):
+        exchanges.append(Exchange(exchange, rank))
+
+    return exchanges
 
 
 @click.command()
-@click.option('--num_coins', prompt='How many coins would you like in the list?')
-@click.option('--num_markets', prompt='How many exchanges would you like to see for each coin?')
-def main(num_coins, num_markets):
+def main():
+    num_coins = click.prompt('How many Cryptos would you like to buy?', type=int)
+    prefered = click.prompt('List your prefered exchanges in order, seperate with a space')
+    prefered = prefered.lower().split()
+
     print('Getting coins...')
     coins = get_coins(num_coins)
+
+    print('Writing coin data to coins.json')
+    to_json(coins, 'coins.json')
+
+    prefered_exchanges = get_prefered_exchanges(prefered)
     for coin in coins:
-        exchanges = get_markets(coin['id'], int(num_markets))
-        coin['markets'] = exchanges
-        coin.pop('id')
+        for exchange in prefered_exchanges:
+            exchange.in_exchange(coin)
 
-    with open('results.json', 'w') as fp:
-        json.dump(coins, fp)
+    print('Writing prefered exchange data to prefered.json')
+    to_json(prefered_exchanges, 'prefered.json', coins=False)
 
-    print('Results in results.json')
     return
 
 
