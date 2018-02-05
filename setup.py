47d7b577e6b3b48d80c27e0b4fbc72c05d55ab1c
rbn920 <robertb.nelson@gmail.com>
2018-02-04 19:36:03 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='bh100',
-      version='0.1.1',
+      version='1.0',
       py_modules='bh100',
-      description='Generates a JSON file of the top Crypto in the market and where you can buy them.',
+      description='Generates a JSON files of the top Crypto in the market and where you can buy them.',
       url='https://github.com/rbn920/bh100',
       author='Robert Nelson',
       author_email='robertb.nelson@gmail.com',
